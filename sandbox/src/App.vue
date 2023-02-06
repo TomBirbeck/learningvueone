2565b68fd1eb779f41150cda0e43d7078b467020
unknown <tombirbeck47@gmail.com>
2023-02-06 16:15:14 +0000

    migrated counter out to it's own component

---
+++ sandbox/src/App.vue
@@ -0,0 +1,60 @@
+<script>
+import BaseCounter from './components/BaseCounter.vue'
+
+export default {
+  components: {
+    BaseCounter,
+  },
+  data() {
+                return {
+                    message: 'Hello it works',
+                    listOfNumbers: [1,2,3,4,5],
+                    listOfWords: ['One', 'Two', 'Three', 'Four'],
+                                    }
+            },
+          
+            }
+</script>
+
+<template>
+  <BaseCounter/>
+  <hr/>
+      <p v-if="message.length % 2 === 0">Even: {{message.toUpperCase()}}</p>
+        <p v-else>Odd: {{message}}</p>
+        <ul>
+            <li v-for="(number, index) in listOfNumbers" v-bind:key="`number-${index}`">{{number}}</li>
+        </ul>
+        <ul>
+            <li v-for="(word, index) in listOfWords" v-bind:key="`word-${index}`">{{word}}</li>
+        </ul>
+        <hr/>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ sandbox/src/components/BaseCounter.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    data: () => ({
+        count: 10,
+        counterTitle: 'Counter Standard',
+        incrementAmount : 8,
+    }),
+    computed: {
+                displayTitle(){
+                    if (this.count > 20) {
+                        return 'Counter Standard - Very Long'
+                    } else {
+                        return 'Counter Standard'
+                    }
+                },
+                optimisedIncrementAmount() {
+                    return this.displayTitle.length * this.incrementAmount
+                }
+            },
+            methods: {
+                incrementCount(){
+                    this.count += this.optimisedIncrementAmount
+                }
+            },
+}
+</script>
+
+<template>
+      <h1>{{displayTitle}}</h1>
+        <p>{{count}}</p>
+        <button v-on:click="incrementCount">increment count</button>
+        <div>
+            <label for="incrementAmount">Increment by:</label>
+            <input type="number"
+            v-model="incrementAmount"
+             />
+        </div>
+        <p>{{incrementAmount}}</p>
+        <p>{{optimisedIncrementAmount}}</p>
+</template>
