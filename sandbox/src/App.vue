783a9d1f64f4099f67067d891c97440dbd692184
TomBirbeck <tombirbeck47@gmail.com>
2023-02-15 14:08:40 +0000

    set up an async request for the pokemon api and connected it up to a button

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,74 +1,24 @@
 <script>
-import BaseCounter from './components/BaseCounter.vue'
-import BaseButton from './components/BaseButton.vue'
-import BaseLayout from './components/BaseLayout.vue'
+// https://pokemon.co/api/v2/pokemon?limit=151
 
 export default {
-  components: {
-    BaseCounter,
-    BaseButton,
-    BaseLayout
+  data: () => ({
+    pokedex: [1, 2, 3],
+  }),
+  methods: {
+    async fetchPokemon() {
+      this.pokedex = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=151"
+      ).then((response) => response.json());
+    },
   },
-  data() {
-                return {
-                    message: 'Hello it works',
-                    listOfNumbers: [1,2,3,4,5],
-                    listOfWords: ['One', 'Two', 'Three', 'Four'],
-                                    }
-            },
-          
-            }
+};
 </script>
 
 <template>
-  <BaseLayout>
-    <template v-slot:sidebar>
-      <BaseButton :left="true">
-      </BaseButton>
-    </template>
-    <template v-slot:main>
-      <hr/>
-          <p v-if="message.length % 2 === 0">Even: {{message.toUpperCase()}}</p>
-            <p v-else>Odd: {{message}}</p>
-            <ul>
-                <li v-for="(number, index) in listOfNumbers" v-bind:key="`number-${index}`">{{number}}</li>
-            </ul>
-    </template>
-    <template #footer>
-      <BaseCounter/>
-    </template>
-  </BaseLayout>
-        <ul>
-            <li v-for="(word, index) in listOfWords" v-bind:key="`word-${index}`">{{word}}</li>
-        </ul>
-        <hr/>
+  <h1>New App</h1>
+  <pre>{{ pokedex }}</pre>
+  <button v-on:click="fetchPokemon">Fetch Pokemon</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
---
+++ sandbox/src/components/scratchpad.vue
@@ -0,0 +1,79 @@
+<script>
+import BaseCounter from "./components/BaseCounter.vue";
+import BaseButton from "./components/BaseButton.vue";
+import BaseLayout from "./components/BaseLayout.vue";
+
+export default {
+  components: {
+    BaseCounter,
+    BaseButton,
+    BaseLayout,
+  },
+  data() {
+    return {
+      message: "Hello it works",
+      listOfNumbers: [1, 2, 3, 4, 5],
+      listOfWords: ["One", "Two", "Three", "Four"],
+    };
+  },
+};
+</script>
+
+<template>
+  <BaseLayout>
+    <template v-slot:sidebar>
+      <BaseButton :left="true"> </BaseButton>
+    </template>
+    <template v-slot:main>
+      <hr />
+      <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
+      <p v-else>Odd: {{ message }}</p>
+      <ul>
+        <li
+          v-for="(number, index) in listOfNumbers"
+          v-bind:key="`number-${index}`"
+        >
+          {{ number }}
+        </li>
+      </ul>
+    </template>
+    <template #footer>
+      <BaseCounter />
+    </template>
+  </BaseLayout>
+  <ul>
+    <li v-for="(word, index) in listOfWords" v-bind:key="`word-${index}`">
+      {{ word }}
+    </li>
+  </ul>
+  <hr />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
