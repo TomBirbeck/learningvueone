42d463f97d864393a92c787cf83077893677fd71
TomBirbeck <tombirbeck47@gmail.com>
2023-02-17 16:28:54 +0000

    adding reactivity to the composition api using ref and reactive

--- sandbox/src/components/Pokedex.vue
+++ sandbox/src/components/Pokedex.vue
@@ -1,15 +1,33 @@
 <script>
+import { ref, computed, reactive } from "vue";
+
 export default {
   async setup() {
-    const regionName = "Europe";
+    const regionName = ref("Europe");
+    const state = reactive({
+      day: "Friday",
+    });
+
+    const dayToAllCaps = state.day.toUpperCase();
+    const regionAllCaps = computed(() => {
+      return regionName.value.toUpperCase();
+    });
+
     const pokedex = await fetch(
       "https://pokeapi.co/api/v2/pokemon?limit=151"
     ).then((response) => response.json());
 
     return {
+      dayToAllCaps,
       pokedex,
       regionName,
+      regionAllCaps,
     };
+  },
+  methods: {
+    changeRegionName() {
+      this.regionName = "Hoenn";
+    },
   },
   created() {
     console.log(this.regionName);
@@ -19,5 +37,9 @@ export default {
 </script>
 
 <template>
+  <h2>{{ regionName }}</h2>
+  <h3>{{ regionAllCaps }}</h3>
+  <h4>{{ dayToAllCaps }}</h4>
+  <button v-on:click="changeRegionName">Change Region Name</button>
   <pre>{{ pokedex }}</pre>
 </template>
